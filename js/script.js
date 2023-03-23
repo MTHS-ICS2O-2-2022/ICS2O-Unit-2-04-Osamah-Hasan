2dacb56c45a6997a99cf242aa8000dffca47fb27
Osamah_Hasan <124389134+Osamah-Hasan@users.noreply.github.com>
2023-03-23 16:34:04 +0000

    update

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Osamah Hasan All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Osamah Hasan
+// Created on: Mar 2023
 // This file contains the JS functions for index.html
 
 'use strict'
 /**
- * This function calculates area and perimeter of rectangle.
+ * This function calculates area and perimeter of Triangle.
  */
 function calculate () {
   // input
-  const H = parseInt(document.getElementById('H of Tringle').value)
-  const A = parseInt(document.getElementById('A of Tringle').value)
-  const B = parseInt(document.getElementById('B of Tringle').value)
-  const C = parseInt(document.getElementById('C of Tringle').value)
+  const H = parseInt(document.getElementById('H of Triangle').value)
+  const A = parseInt(document.getElementById('A of Triangle').value)
+  const B = parseInt(document.getElementById('B of Triangle').value)
+  const C = parseInt(document.getElementById('C of Triangle').value)
 
   // process
   const area = H * B / 2
